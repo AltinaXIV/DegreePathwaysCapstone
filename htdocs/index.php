52bc327ee931fea6c9bb059beadf10c921f03f18
Giovanni Panaro <tsrwebgl@gmail.com>
2020-02-23 12:20:32 -0500

    Build basic top-nav bar. Non-functional

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,8 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>University of Pittsburgh Degree Pathway Tool</title>
+        <link rel="stylesheet" href="css/default.css">
     </head>
     <body>
-
+        <header class="material-nav-bar">
+            <button class="menu-button"><img height="48" width="48" alt="menu" src="/icon/menu.svg"></button>
+        </header>
     </body>
 </html>
