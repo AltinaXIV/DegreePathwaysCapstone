2ad08b66077113e9624fca96f3d37f1f9896c460
jmc282 <jmc282@pitt.edu>
2020-02-24 16:59:38 -0500

    revisions

--- htdocs/js/columns.js
+++ htdocs/js/columns.js
@@ -1,23 +1,32 @@
-function addRootTo($id){
-    var elem = document.getElementById($id);
-    elem.innerHTML="<root-class-bubble></root-class-bubble>";
+// add a bubble to a given element (column) by id
+function addRootTo(id){
+    var elem = document.getElementById(id);
+    var bubble = document.createElement("root-class-bubble");
+    elem.appendChild(bubble);
 }
 
-function addBranchTo($id){
-    var elem = document.getElementById($id);
-    elem.innerHTML="<branch-class-bubble></branch-class-bubble>";
+function addBranchTo(id){
+    var elem = document.getElementById(id);
+    var bubble = document.createElement("branch-class-bubble");
+    elem.appendChild(bubble);
 }
 
-function addLeafTo($id){
-    var elem = document.getElementById($id);
-    elem.innerHTML="<leaf-class-bubble></leaf-class-bubble>";
+function addLeafTo(id){
+    var elem = document.getElementById(id);
+    var bubble = document.createElement("leaf-class-bubble");
+    elem.appendChild(bubble);
 }
 
-function addStandAloneTo($id){
-    var elem = document.getElementById($id);
-    elem.innerHTML="<stand-alone-class-bubble></stand-alone-class-bubble>";
+function addStandAloneTo(id){
+    var elem = document.getElementById(id);
+    var bubble = document.createElement("stand-alone-class-bubble");
+    elem.appendChild(bubble);
 }
 
+
+// creates the number of columns specified in input field id="semesters"
+// columns are appended to EXISTING row id="main-body" on the page
+// gives each column id="column-i"
 function generateColumns(){
     var row = document.getElementById("main-body");
     row.innerHTML='';
