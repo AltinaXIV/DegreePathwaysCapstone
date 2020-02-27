d7cbc973c3d5738438197b50385dbf3e46da0982
jmc282 <jmc282@pitt.edu>
2020-02-27 00:49:54 -0500

    body now a table instead of columns. demo-able cs minor positioning. issues with flexible sizing- use 3 semesters

--- htdocs/js/columns.js
+++ htdocs/js/columns.js
@@ -1,8 +1,8 @@
 // add a bubble to a given element (column) by id
 // temporarily places the argument course_no into the course-number slot
 // needs work and real data
-function addRootTo(column, course_no){
-    var elem = document.getElementById(column, course_no);
+function addRootTo(id, course_no){
+    var elem = document.getElementById(id, course_no);
     var bubble = document.createElement("root-class-bubble");
     bubble.id = course_no;
     bubble.innerHTML= '<div slot="course-number">' + course_no + '</div>';
@@ -10,8 +10,8 @@ function addRootTo(column, course_no){
     elem.appendChild(document.createElement("BR"));
 }
 
-function addBranchTo(column, course_no){
-    var elem = document.getElementById(column, course_no);
+function addBranchTo(id, course_no){
+    var elem = document.getElementById(id, course_no);
     var bubble = document.createElement("branch-class-bubble");
     bubble.id = course_no;
     bubble.innerHTML= '<div slot="course-number">' + course_no + '</div>';
@@ -19,8 +19,8 @@ function addBranchTo(column, course_no){
     elem.appendChild(document.createElement("BR"));
 }
 
-function addLeafTo(column, course_no){
-    var elem = document.getElementById(column, course_no);
+function addLeafTo(id, course_no){
+    var elem = document.getElementById(id, course_no);
     var bubble = document.createElement("leaf-class-bubble");
     bubble.id = course_no;
     bubble.innerHTML= '<div slot="course-number">' + course_no + '</div>';
@@ -28,8 +28,8 @@ function addLeafTo(column, course_no){
     elem.appendChild(document.createElement("BR"));
 }
 
-function addStandAloneTo(column, course_no){
-    var elem = document.getElementById(column, course_no);
+function addStandAloneTo(id, course_no){
+    var elem = document.getElementById(id, course_no);
     var bubble = document.createElement("stand-alone-class-bubble");
     bubble.id = course_no;
     bubble.innerHTML= '<div slot="course-number">' + course_no + '</div>';
@@ -37,7 +37,53 @@ function addStandAloneTo(column, course_no){
     elem.appendChild(document.createElement("BR"));
 }
 
+// just a test function to place sample data
+function place(){
+    addRootTo("r1-c1", "MATH");
+    addBranchTo("r1-c2", "CS441");
+    addBranchTo("r3-c2", "CS445");
+    addLeafTo("r2-c3", "CS1501");
+    addRootTo("r3-c1","CS401");
+    addLeafTo("r3-c3", "CS449");
+    addLeafTo("r4-c2", "CS447");
+}
+
+function drawTable() {
+    var rows = 10; //specify as 10 for now
+    var columns = parseInt(document.getElementById("semesters").value);
+    
+    var body = document.getElementById('main-body');
+    body.innerHTML='';
+    var table = document.createElement("table");
+    table.id = "pathway";
+
+    for (var r = 0; r <= rows; r++) {
+        var row = document.createElement("tr");
+        for (var c = 1; c <= columns; c++) {
+            var cell = document.createElement("td");
+            if (r == 0){
+                cell.innerHTML ="Semester " + c;
+                cell.id = "semester-" + c + "-label";
+            }
+            else {
+                cell.id= "r" + r + "-c" + c;
+                // cell.innerHTML = "r" + r + "-c" + c;
+            }
+            row.appendChild(cell);
+        }           
+    table.appendChild(row);
+    }
+    body.appendChild(table);
+    
+}
+
+function demo(){
+    drawTable();
+    place();
+}
+//--------------------------------------------------------------------------
 /**
+ * drawTable is working better for placing bubbles
  * creates the number of columns specified in input field id="semesters"
  * columns are appended to EXISTING row id="main-body" on the page
  * gives each column id="column-i"
@@ -54,29 +100,44 @@ function generateColumns(){
         var semester_label = document.createElement("P");
         semester_label.innerHTML = "Semester " + i;
         col.appendChild(semester_label);
-        // col.innerHTML +="<p>Semester " + i + "</p>";
         col.id = "column-" + i;
         row.appendChild(col);
     }
-    // place(semesters);
+    // place();
 }
 
-// just a test function to place sample data
-function place(semesters){
-    addRootTo("column-1", "cs401");
-    addRootTo("column-1","MATH");
-    addBranchTo("column-2", "cs445");
-    addBranchTo("column-2", "cs441");
-    addLeafTo("column-3", "cs447");
-    addLeafTo("column-3", "cs449");
-    addLeafTo("column-3", "cs1501");
-}
-
-function test(){
-    var row = document.getElementById("main-body").children;
-    for (i = 0; i < row.length; i++) {
-        row[i].innerHTML= 'Semester ' + (i+1) + '<br>';
-    }
+/**
+ * Modifications to column generation by adding cells to columns. 
+ * Table is working better, ignore for now
+ * creates the number of columns specified in input field id="semesters"
+ * columns are appended to EXISTING row id="main-body" on the page
+ * gives each column id="column-i"
+ */
+function generateColumns2(){
+    var row = document.getElementById("main-body");
+    row.innerHTML='';
+    row.className="row pathway-body";
     var semesters = parseInt(document.getElementById("semesters").value);
-    place(semesters);
+    for (c = 1; c <= semesters; c++) {
+        var col = document.createElement("div");
+        col.className = "column pathway-body";
+
+        for (r = 0; r <= 8; r++) {
+            var cell = document.createElement("cell");
+            if (r == 0){
+                cell.innerHTML ="Semester " + c;
+                cell.id = "semester-" + c + "-label";
+            }
+            else {
+                cell.id= "r" + r + "-c" + c;
+                // cell.innerHTML = "r" + r + "-c" + c;
+            }
+            col.appendChild(cell);
+            col.appendChild(document.createElement("BR"));
+         }
+
+        col.id = "column-" + c;
+        row.appendChild(col);
+    }
+    place();
 }
