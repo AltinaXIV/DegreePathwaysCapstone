6db6eeb886214b71302512abb6e860b21734158f
Giovanni Panaro <tsrwebgl@gmail.com>
2020-04-08 14:38:26 -0400

    Minor bug fixes

--- htdocs/index.php
+++ htdocs/index.php
@@ -12,7 +12,7 @@
     <body>
         <?php include './template/bubbleElements.html' ?>
         <header class="material-nav-bar">
-            <button class="menu-button" ripple="ripple" onclick="activateLeftNav()"><img height="50" width="50" alt="menu" src="/icon/menu.svg"></button>
+            <button class="menu-button" ripple="ripple" onclick="activateLeftNav()"><img height="49" width="49" alt="menu" src="/icons/menu.svg"></button>
         </header>
         <div class="row">
             <div class="left-nav-container" id="left-nav">
