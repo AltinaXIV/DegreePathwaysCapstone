301ed5a4bec715bc2ee902bac0c1ccc585b9aca5
Giovanni Panaro <tsrwebgl@gmail.com>
2020-04-09 17:53:42 -0400

    Javascript can now assign placement values to classes

--- htdocs/js/pathways.js
+++ htdocs/js/pathways.js
@@ -3,9 +3,31 @@ let enableCompactView = false;
 let Info = class {
     constructor() {
         this.classes = {};
-        this.prereqs = {};
-        this.coreqs = {};
+        this.prereqs = new Map();
+        this.coreqs = new Map();
         this.minSemesters = 0;
+        /**
+         * @type {Map<number, number[]>} Maps a placement value to an array of class IDs.
+         * The placement value shows where it needs to be placed on the pathway. Anything labeled 0 needs to be placed,
+         * 1 second, etc.
+         *
+         * e.g. For the CS Minor:
+         * 0 -> [220, 401, 441, 449]
+         * 1 -> [445, 447]
+         * 2 -> [1501]
+         */
+        this.valuesArray = new Map();
+        /**
+         * @type {Map<number, number>} Just like above but reversed
+         *
+         * e.g.
+         * 220 -> 0
+         * 401 -> 0
+         * ...
+         * 447 -> 1
+         * 1501 -> 2
+         */
+        this.valuesArrayReverse = new Map();
     };
 };
 
@@ -85,7 +107,39 @@ function addStandAloneTo(id, course_no, course_name){
     elem.appendChild(document.createElement("br"));
 }
 
+/**
+ * @param courseID {number} course ID
+ * @param info {Info} info
+ */
+function calculateDepth(courseID, info) {
+    let maxDepth = 0;
 
+    if(info.valuesArrayReverse.has(courseID)) {
+        return info.valuesArrayReverse.get(courseID);
+    }
+
+    // Get the prereqs and iterate through them
+    let prereqs = info.prereqs.get(courseID);
+    if(prereqs !== undefined) {
+        for(let i = 0; i < prereqs.length; i++) {
+            let d = calculateDepth(prereqs[i], info) + 1;
+            maxDepth = Math.max(maxDepth, d);
+        }
+    }
+
+    // And now co-reqs...
+    let coreqs = info.coreqs.get(courseID);
+    if(coreqs !== undefined) {
+        for(let i = 0; i < coreqs.length; i++) {
+            let d = calculateDepth(coreqs[i], info);
+            maxDepth = Math.max(maxDepth, d);
+        }
+    }
+
+    info.valuesArrayReverse.set(courseID, maxDepth);
+    return maxDepth;
+
+}
 
 /**
  * Get information needed to map out course
@@ -97,19 +151,51 @@ async function getInfo() {
     let r = await fetch("/cdn/query.php?a=major");
     info.classes = await r.json();
     r = await fetch("/cdn/query.php?a=prereq");
-    info.prereqs = await r.json();
+    let prereqs;
+    prereqs = await r.json();
     r = await fetch("/cdn/query.php?a=coreq");
-    info.coreqs = await r.json();
+    let coreqs;
+    coreqs = await r.json();
 
     info.classes = info.classes['classes'];
-    info.prereqs = info.prereqs['prereqs'];
-    info.coreqs = info.coreqs['coreqs'];
+    prereqs = prereqs['prereqs'];
+    coreqs = coreqs['coreqs'];
+
+    console.log(prereqs);
+
+    // Calculate minimum number of semesters needed to finish.
+
+    // Step 1: organize prereqs and coreqs into an map.
+    for(let i = 0; i < prereqs.length; i++) {
+        if(info.prereqs.get(prereqs[i]['course']) === undefined) {
+            info.prereqs.set(prereqs[i]['course'], [prereqs[i]['prereq']]);
+        } else {
+            let v = info.prereqs.get(prereqs[i]['course']);
+            info.prereqs.set(prereqs[i]['course'], v.concat([prereqs[i]['prereq']]))
+        }
+    }
+
+    for(let i = 0; i < coreqs.length; i++) {
+        if(info.coreqs.get(coreqs[i]['course']) === undefined) {
+            info.coreqs.set(coreqs[i]['course'], [coreqs[i]['coreq']]);
+        } else {
+            let v = info.coreqs.get(coreqs[i]['course']);
+            info.prereqs.set(coreqs[i]['course'], v.concat([coreqs[i]['coreq']]))
+        }
+    }
+
+    // Step 2: Think of classes as a graph. For each class, find the quickest path to the Root.
+    // Todo: This will re-evaluate values many times. Make this more efficient. gip20@pitt.edu
+
+    for(let i = 0; i < info.classes.length; i++) {
+        calculateDepth(info.classes[i]['id'], info)
+    }
+
     return info;
 }
 
 async function demo() {
     let info = await getInfo();
-    console.log(info);
     drawTable();
     //place();
 }
